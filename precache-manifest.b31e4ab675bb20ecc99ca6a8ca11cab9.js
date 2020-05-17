dfd68f119ffce8d62bcd55834fe5386c157dae57
singh-param <43931784+singh-param@users.noreply.github.com>
2020-05-17 07:27:59 +0530

    Updates

---
+++ precache-manifest.b31e4ab675bb20ecc99ca6a8ca11cab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61dfeb8e07b50daa7f7995f84423bb51",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ebadfc5c7c1859ff447c",
+    "url": "/contactmanager/static/css/2.817c6c0f.chunk.css"
+  },
+  {
+    "revision": "bbdc54686542f70d0145",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ebadfc5c7c1859ff447c",
+    "url": "/contactmanager/static/js/2.126153af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.126153af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbdc54686542f70d0145",
+    "url": "/contactmanager/static/js/main.89e28bb9.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
